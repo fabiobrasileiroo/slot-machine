ad04979b514754a9b8545c3a38fe0fe0c776fb9d
fabiobrasileiroo <fabio.h591@gmail.com>
2024-02-29 18:32:10 -0400

    win50

--- script1.js
+++ script1.js
@@ -17,7 +17,7 @@ window.raf = (function () {
       height: "200",
       colNum: 3,
       rowNum: 9,
-      winRate: 100,
+      winRate: 50,
       autoPlay: false,
       autoSize: false,
       autoPlayTime: 10,
